12b5e734863d4596f75155884ddd5cbb680500ea
AbhVik <138740399+AbhVik@users.noreply.github.com>
2026-07-31 08:35:06 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE='laxmi-khata-v9';const FILES=['./','./index.html','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
